352049cb8d4ceaa13085c7adca2dfab8317b0f6b
timmy <timmy.caicai@gmail.com>
2020-11-04 18:18:22 +0800

    增加并发

--- concurrent/concurrent.go
+++ concurrent/concurrent.go
@@ -3,20 +3,19 @@ package main
 import (
 	"fmt"
 	"sync"
-	"time"
 )
 
 var wg sync.WaitGroup // 1
 
 func main() {
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 100; i++ {
+		wg.Add(1) // 2位置一定要放在这里
 		go func(i int) {
-			wg.Add(1)       // 2
-			defer wg.Done() // 3
+			defer wg.Done() // 3位置一定要放在这里
 			fmt.Printf("do some thing, index:[%d] \n", i)
-			time.Sleep(3 * time.Second)
+			//time.Sleep(3 * time.Second)
 		}(i) // *
 	}
-	wg.Wait() // 4
+	wg.Wait() // 4位置一定要放在这里
 	fmt.Println("main finished")
 }
