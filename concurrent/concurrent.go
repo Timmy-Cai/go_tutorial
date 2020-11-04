3abc1eb874f87fa1ee7ea502f3826ac3d5ce58bf
timmy <timmy.caicai@gmail.com>
2020-11-04 17:34:56 +0800

    增加并发

--- concurrent/concurrent.go
+++ concurrent/concurrent.go
@@ -10,8 +10,8 @@ var wg sync.WaitGroup // 1
 
 func main() {
 	for i := 0; i < 10; i++ {
-		wg.Add(1) // 2
 		go func(i int) {
+			wg.Add(1)       // 2
 			defer wg.Done() // 3
 			fmt.Printf("do some thing, index:[%d] \n", i)
 			time.Sleep(3 * time.Second)
